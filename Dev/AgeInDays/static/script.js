846ead395df6da8c92db25be4ec75fec50c9e38f
Namala Prathyusha <namalaprathyusha1@gmail.com>
2025-02-15 20:48:45 +0530

    Age In Days Project Complete

--- Dev/AgeInDays/static/script.js
+++ Dev/AgeInDays/static/script.js
@@ -1,1 +1,23 @@
-alert("Hello!!");
+// alert("Hello!!");
+// console.log('Hello World!!')
+
+//challenge 1: Age in Days
+
+function ageInDays(){
+    if(document.getElementById('ageInDays')){
+        document.getElementById('ageInDays').remove();
+    }
+    var birthYear = prompt('What year were you born....?');
+    var age = (2025-birthYear)*365;
+
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are '+age+ ' days old...!');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+//     console.log(age);
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
