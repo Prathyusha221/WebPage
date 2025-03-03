e301dce23d5fedf87a7cebae74b3fb84831836e0
Namala Prathyusha <namalaprathyusha1@gmail.com>
2025-03-03 23:05:11 +0530

    Blackjack completed game with js

--- Dev/ChangeBtnColor/static/script.js
+++ Dev/ChangeBtnColor/static/script.js
@@ -181,3 +181,216 @@ function buttonRandom(){
         all_buttons[i].classList.add(choices[num]);
     }
 }
+
+
+//Challenge 5: BlackJack Game
+let blackjackGame = {
+    'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+    'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+    'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
+    'cardsMap': {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': [1,11]},
+    'wins': 0,
+    'losses': 0,
+    'draws': 0,
+};
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+
+const hitSound = new Audio('static/sounds/swish.m4a');
+const winSound = new Audio('static/sounds/cash.mp3');
+const lostSound = new Audio('static/sounds/aww.mp3');
+
+document.getElementById('blackjack-stand-button').disabled = true;
+document.getElementById('blackjack-stand-button').style.opacity = 0.5;
+document.getElementById('blackjack-deal-button').disabled = true;
+document.getElementById('blackjack-deal-button').style.opacity = 0.5;
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+document.querySelector('#blackjack-stand-button').addEventListener('click', blackjackStand);
+
+function blackjackHit(){
+    // alert('Ouch, you just clicked me!');
+    if(YOU['score']<21){
+        let card=randomCard();
+        showCard(YOU, card);
+        updateScore(YOU, card);
+        showScore(YOU);
+        // console.log(YOU['score']);
+    }
+
+    if(YOU['score']>0){
+        document.getElementById('blackjack-stand-button').disabled = false;
+        document.getElementById('blackjack-stand-button').style.opacity = 1;
+    }
+}
+
+function randomCard(){
+    let randomIndex=Math.floor(Math.random()*13);
+    return blackjackGame['cards'][randomIndex];
+}
+
+function showCard(activePlayer, card){
+    if(activePlayer['score']<=21){
+        let cardImage = document.createElement('img');
+        // cardImage.src = 'static/images/'+card+'.png';
+        cardImage.src = `static/images/${card}.png`;
+        document.querySelector(activePlayer['div']).appendChild(cardImage);
+        hitSound.play();
+    }
+}
+
+function updateScore(activePlayer, card){
+    //if adding 11 keeps me below 21 then add 11 else 1
+    if(card=='A'){
+        if(activePlayer['score']+blackjackGame['cardsMap'][card][1]<=21){
+            activePlayer['score']+=blackjackGame['cardsMap'][card][1];
+        }
+        else{
+            activePlayer['score']+=blackjackGame['cardsMap'][card][0];
+        }
+    }
+    else{
+        activePlayer['score']+=blackjackGame['cardsMap'][card];
+    }
+}
+
+function showScore(activePlayer){
+    if(activePlayer['score']>21){
+        document.querySelector(activePlayer['scoreSpan']).innerHTML = 'BUST!';
+        document.querySelector(activePlayer['scoreSpan']).style.color = 'red';
+    }
+    else{
+        document.querySelector(activePlayer['scoreSpan']).innerHTML=activePlayer['score'];
+    }
+}
+
+async function blackjackStand(){
+    // alert('Ouch, you just clicked me!');
+    document.getElementById('blackjack-hit-button').disabled = true;
+    document.getElementById('blackjack-hit-button').style.opacity = 0.5;
+    
+    while(DEALER['score']<16){
+        let card=randomCard();
+        showCard(DEALER, card);
+        updateScore(DEALER, card);
+        showScore(DEALER);
+        await sleep(750);
+    }
+
+    let winner=computeWinner();
+    showResult(winner);
+
+    // Disable Hit and Stand buttons
+    document.getElementById('blackjack-hit-button').disabled = true;
+    document.getElementById('blackjack-stand-button').disabled = true;
+    // Optionally, you can visually grey them out too
+    document.getElementById('blackjack-hit-button').style.opacity = 0.5;
+    document.getElementById('blackjack-stand-button').style.opacity = 0.5;
+
+    //Enabling Deal
+    document.getElementById('blackjack-deal-button').disabled = false;
+    document.getElementById('blackjack-deal-button').style.opacity = 1;
+
+    // showResult(computeWinner());
+}
+
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+
+//compute winner and return who won
+//update the wins, draws and losses
+function computeWinner(){
+    let winner;
+
+    if(YOU['score']<=21){
+        //condition: higher score than dealer or when dealer busts but you're 21 or under
+        if(YOU['score']>DEALER['score'] || DEALER['score']>21){
+            blackjackGame['wins']++;
+            winner=YOU;
+        }
+        else if(YOU['score']<DEALER['score']){
+            blackjackGame['losses']++;
+            winner=DEALER;
+        }
+        else if(YOU['score']==DEALER['score']){
+            blackjackGame['draws']++;
+        }
+    }
+    //you bust but dealer doesn't
+    else if(YOU['score']>21 && DEALER['score']<=21){
+        blackjackGame['losses']++;
+        winner=DEALER;
+    }
+    //when both busts
+    else if(YOU['score']>21 && DEALER['score']>21){
+        blackjackGame['draws']++;
+    }
+
+    return winner;
+}
+
+function showResult(winner){
+    if(winner==YOU){
+        document.querySelector('#blackjack-result').textContent='You Won!';
+        document.querySelector('#blackjack-result').style.color = 'green';
+        document.querySelector('#wins').textContent = blackjackGame['wins'];
+        winSound.play();
+    }
+    else if(winner==DEALER){
+        document.querySelector('#blackjack-result').textContent='You Lost!';
+        document.querySelector('#blackjack-result').style.color = 'red';
+        document.querySelector('#losses').textContent = blackjackGame['losses'];
+        lostSound.play();
+    }
+    else{
+        document.querySelector('#blackjack-result').textContent='You Drew!';
+        document.querySelector('#draws').textContent = blackjackGame['draws'];
+        // winSound.play();
+    }
+}
+
+function blackjackDeal(){
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+    
+    for(let i=0;i<yourImages.length;i++){
+        yourImages[i].remove();
+    }
+
+    for(let i=0;i<dealerImages.length;i++){
+        dealerImages[i].remove();
+    }
+
+    YOU['score']=0;
+    DEALER['score']=0;
+    document.querySelector(YOU['scoreSpan']).innerHTML=0;
+    document.querySelector(YOU['scoreSpan']).style.color = 'white';
+    document.querySelector(DEALER['scoreSpan']).innerHTML=0;
+    document.querySelector(DEALER['scoreSpan']).style.color = 'white';
+    
+
+
+    document.querySelector('#blackjack-result').innerHTML = "Let's Play";
+    document.querySelector('#blackjack-result').style.color = 'black';
+    
+    
+    // Re-enable Hit and Stand buttons
+    document.getElementById('blackjack-hit-button').disabled = false;
+    document.getElementById('blackjack-deal-button').disabled = true;
+    // Reset button appearance
+    document.getElementById('blackjack-hit-button').style.opacity = 1;
+    document.getElementById('blackjack-deal-button').style.opacity = 0.5;
+    
+    
+    // blackjackGame['wins']=0;
+    // blackjackGame['losses']=0;
+    // blackjackGame['draws']=0;
+
+    // document.querySelector('#wins').textContent = blackjackGame['wins'];
+    // document.querySelector('#losses').textContent = blackjackGame['losses'];
+    // document.querySelector('#draws').textContent = blackjackGame['draws'];
+}
